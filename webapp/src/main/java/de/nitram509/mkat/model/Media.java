a886409f4ebebb850619036e5b284ad50954803e
nitram509 <maki@bitkings.de>
2014-01-03 15:46:15 +0100

    backend models shouldn't be JAXB annotated

--- webapp/src/main/java/de/nitram509/mkat/model/Media.java
+++ webapp/src/main/java/de/nitram509/mkat/model/Media.java
@@ -1,44 +1,19 @@
 package de.nitram509.mkat.model;
 
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
-import java.beans.Transient;
 import java.util.Date;
 
-@XmlRootElement(name = "media")
 public class Media implements Cloneable {
 
-  @XmlElement(name = "id")
   public int media_id;
-
-  @XmlElement(name = "group_id")
   public String group_id;
-
-  @XmlElement(name = "movie_id")
   public int movie_id;
-
-  @XmlElement(name = "name")
   public String name;
-
-  @XmlElement(name = "label")
   public String label;
-
-  @XmlElement(name = "size")
   public long size;
-
-  @XmlElement(name = "updated")
   public Date updated;
-
-  @XmlElement(name = "description")
   public String description;
-
-  @XmlElement(name = "languages")
   public int languages;
-
-  @XmlElement(name = "media_type")
   public int media_type;
-
-  @XmlElement(name = "numberOfFiles")
   public int numberOfFiles;
 
   public Media() {
--- webapp/src/main/java/de/nitram509/mkat/model/MkatFile.java
+++ webapp/src/main/java/de/nitram509/mkat/model/MkatFile.java
@@ -1,33 +1,15 @@
 package de.nitram509.mkat.model;
 
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
 import java.util.Date;
 
-@XmlRootElement(name = "File")
 public class MkatFile {
 
-  @XmlElement(name = "id")
   public long id = 0;
-
-  @XmlElement(name = "media_id")
   public long media_id;
-
-  @XmlElement(name = "name")
   public String name;
-
-  @XmlElement(name = "size")
   public long size;
-
-  @XmlElement(name = "datetime")
   public Date datetime;
-
-  @XmlElement(name = "notizen")
   public String notizen;
-
-  @XmlElement(name = "isdir")
   public boolean isdir;
-
-  @XmlElement(name = "md5")
   public String md5;
 }
